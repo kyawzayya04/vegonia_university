376173d123272b08456ffa355a85f491e151c6f8
Kyaw Zay Ya <kyawzayya.blem@gmail.com>
2022-10-14 16:01:04 +0630

    vegonia university

--- js/app.js
+++ js/app.js
@@ -7,6 +7,19 @@ $(document).ready(function(){
 	})
 		// End Navbar
 	// End Header
+
+	// Start Login Box
+
+	$("#openform").click(function(){
+		document.getElementById("form-popup").style.display = "block";
+	})
+
+	$("#formclose-btn").click(function(){
+		document.getElementById("form-popup").style.display = "none"
+	})
+
+	// End Login Box
+
 })
 
 
@@ -44,5 +57,43 @@ getcountervalues.forEach(function(getcountervalue){
 
 // End Student Counter
 
+// Start Rating Section
+
+// start google code for chart
+
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+
+        var data = google.visualization.arrayToDataTable([
+          ['Task', 'Hours per Day'],
+          ['Myanmar',     8],
+          ['Thailand',      2],
+          ['Singapore',  4],
+          ['Indonesia', 2],
+          ['Srilanka',    8]
+        ]);
+
+        var options = {
+          title: 'International Students',
+          is3D:true,
+          // pieHole:0.4,
+          // width:550,
+          // height:400
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+        chart.draw(data, options);
+      }
+
+// end google code for chart
+
+// End Rating Section
+
+const getyear = document.getElementById("getyear");
+const getfullyear = new Date().getUTCFullYear();
+getyear.textContent = getfullyear;
 
 // End Javascript Area
